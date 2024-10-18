3df0c67f080b42f772227ba7f258f9750d150733
apeltop <sunshine@ptokos.com>
2024-10-18 14:25:15 +0900

    menus router 에 GET /menus/menuId 추가하였다.

--- routes/menus.js
+++ routes/menus.js
@@ -11,31 +11,43 @@ router.get('/stats', (req, res, next) => {
     });
 });
 
+const menus = [
+    {
+        id: 1,
+        name: 'Latte',
+        type: 'Coffee',
+        temperature: 'hot',
+        price: 4500,
+        totalOrders: 5
+    },
+    {
+        id: 2,
+        name: 'Iced Tea',
+        type: 'Tea',
+        temperature: 'ice',
+        price: 3000,
+        totalOrders: 10
+    }
+];
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        menus: [
-            {
-                id: 1,
-                name: 'Latte',
-                type: 'Coffee',
-                temperature: 'hot',
-                price: 4500,
-                totalOrders: 5
-            },
-            {
-                id: 2,
-                name: 'Iced Tea',
-                type: 'Tea',
-                temperature: 'ice',
-                price: 3000,
-                totalOrders: 10
-            }
-        ]
+        menus
+    });
+});
+
+router.get('/:menuId', (req, res, next) => {
+    const id = req.params.menuId;
+    const menu = menus[0];
+
+    res.status(200).json({
+        menu
     });
 });
 
 router.post('/', (req, res, next) => {
     console.log(req.body);
+
     res.status(201).json({
         message: '메뉴 생성되었습니다.',
         menu: {
@@ -47,6 +59,7 @@ router.post('/', (req, res, next) => {
 router.put('/:menuId', (req, res, next) => {
     const id = req.params.menuId;
     console.log(req.body);
+
     res.status(200).json({
         message: `메뉴 ${id} 수정되었습니다.`
     });
@@ -54,6 +67,8 @@ router.put('/:menuId', (req, res, next) => {
 
 router.delete('/:menuId', (req, res, next) => {
     const id = req.params.menuId;
+    console.log(req.body)
+
     res.status(200).json({
         message: `메뉴 ${id} 삭제되었습니다.`
     });
